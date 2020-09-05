f739c70bc22f9d2c7996c85e565ab6f6551da90e
Edixon Piña <edixonalbertto@gmail.com>
2020-09-05 19:05:48 -0400

    adding structure control

--- src/estructure_control.ex
+++ src/estructure_control.ex
@@ -8,4 +8,33 @@ defmodule EstructureControl do
     end
   end
 
+  def my_unless(condition) do
+    unless condition do
+      "falso"
+    end
+  end
+
+  def my_cond(condition) do
+    cond do
+      condition > 5 -> "> 5"
+      condition < 5 -> "< 5"
+      true -> "false"
+    end
+  end
+
+  def my_case() do
+    case condition do
+      {:ok, data} when is_integer(data) -> "Ok: #{data}"
+      {:error, msg} -> "Error: #{msg}"
+      _ -> "Not match"
+    end
+  end
+
+  def mi_with(condition) do
+    with {:ok %{"data" => data}, age} <- condition,
+      [name: name, lastname: lastname] <- data do
+      "#{name} #{lastname} tiene #{age} años"
+    end
+  end
+
 end
